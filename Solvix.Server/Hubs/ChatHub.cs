f86e613ace1b92a286c551c98bd022f795b45bbf
AAK <AAK@DESKTOP-BRS7AE0>
2025-04-13 16:17:00 +0330

    افزودن قابلیت ارسال و دریافت پیام اما فعلا به شرطی که هر دو طرف آنلاین باشن

--- Solvix.Server/Hubs/ChatHub.cs
+++ Solvix.Server/Hubs/ChatHub.cs
@@ -8,13 +8,20 @@ namespace Solvix.Server.Hubs
     {
         private static ConcurrentDictionary<string, string> UserConnections = new();
 
+
         public override async Task OnConnectedAsync()
         {
             var httpContext = Context.GetHttpContext();
 
             if (httpContext != null && httpContext.Request.Query.TryGetValue("user", out var username))
             {
-                UserConnections[username] = Context.ConnectionId;
+                if (!string.IsNullOrEmpty(username))
+                {
+                    UserConnections[username] = Context.ConnectionId;
+                    await Clients.Caller.SendAsync("UserConnected", username);
+                    await SendConnectedUsers();
+                }
+
             }
             await base.OnConnectedAsync();
         }
@@ -25,19 +32,32 @@ namespace Solvix.Server.Hubs
             if (!string.IsNullOrEmpty(username))
             {
                 UserConnections.TryRemove(username, out _);
+                await Clients.All.SendAsync("UserDisconnected", username);
             }
             await base.OnDisconnectedAsync(exception);
         }
+
 
         public async Task SendPrivateMessage(string recipientUsername, string message)
         {
             string senderUsername = UserConnections.FirstOrDefault(x => x.Value == Context.ConnectionId).Key;
             if (string.IsNullOrEmpty(senderUsername))
                 return;
+
             if (UserConnections.TryGetValue(recipientUsername, out string recipientConnectionId))
             {
                 await Clients.Client(recipientConnectionId).SendAsync("ReceiveMessage", senderUsername, message);
             }
+            else
+            {
+                await Clients.Caller.SendAsync("ErrorMessage", $"کاربر '{recipientUsername}' در حال حاضر آنلاین نیست.");
+            }
+        }
+
+
+        private async Task SendConnectedUsers()
+        {
+            await Clients.All.SendAsync("ConnectedUsers", UserConnections.Keys.ToList());
         }
     }
 }

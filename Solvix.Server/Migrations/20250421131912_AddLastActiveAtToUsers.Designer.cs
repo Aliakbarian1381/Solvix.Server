ccd3b9b2060f55528ac15874176ef54a8970e008
AAK <AAK@DESKTOP-BRS7AE0>
2025-04-21 16:52:38 +0330

    تغییر کوچیک در دیتابیس

--- Solvix.Server/Migrations/20250421131912_AddLastActiveAtToUsers.Designer.cs
+++ Solvix.Server/Migrations/20250421131912_AddLastActiveAtToUsers.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Solvix.Server.Data;
@@ -11,9 +12,11 @@ using Solvix.Server.Data;
 namespace Solvix.Server.Migrations
 {
     [DbContext(typeof(ChatDbContext))]
-    partial class ChatDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250421131912_AddLastActiveAtToUsers")]
+    partial class AddLastActiveAtToUsers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

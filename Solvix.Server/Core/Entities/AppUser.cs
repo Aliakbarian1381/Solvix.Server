3399a41c855bf7b3af13f7ca892e9f3769704cb4
MohammadMahdi Aliakbarian <mahdi.aliakbarian2@gmail.com>
2025-07-18 21:06:45 +0330

    ...

--- Solvix.Server/Core/Entities/AppUser.cs
+++ Solvix.Server/Core/Entities/AppUser.cs
@@ -9,9 +9,13 @@ namespace Solvix.Server.Core.Entities
         public string? LastName { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime? LastActiveAt { get; set; }
-        public DateTime? LastSeenAt { get; set; }  // ⭐ اضافه شد
+        public DateTime? LastSeenAt { get; set; }
         public string? FcmToken { get; set; }
         public string? ProfilePictureUrl { get; set; }
+
+        public bool IsOnline { get; set; } = false;
+
+        public string Username => UserName ?? "";
 
         public virtual ICollection<Message> SentMessages { get; set; } = new HashSet<Message>();
         public virtual ICollection<UserConnection> Connections { get; set; } = new HashSet<UserConnection>();
